3dfa5ad10e0330d650be4b8cdf1dcec9b8ece4d8
yangzq <yangzq@infore.com>
2023-07-31 16:52:52 +0800

    chore: 新增依赖sass和reset.css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,12 @@ import HelloWorld from '../components/HelloWorld.vue'
 
 <template>
   <main>
-    <HelloWorld />
+    <HelloWorld class="main" />
   </main>
 </template>
+
+<style lang="scss">
+.main {
+  color: Red;
+}
+</style>
