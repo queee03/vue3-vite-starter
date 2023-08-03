eab0fd29a859f76d9752926ef00233008a077fdc
yangzq <yangzq@infore.com>
2023-08-03 18:03:20 +0800

    chore: 引入约定式路由插件；支持import语句排序

---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<!-- <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+router.push('/')
+</script> -->
+
+<route lang="json">
+{
+  "name": "home",
+  "redirect": "/"
+}
+</route>
---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="main">hello hello</main>
+</template>
+
+<style lang="scss">
+.main {
+  color: Red;
+}
+</style>
